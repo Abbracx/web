9896ac6b7e4a37c948cf74e8bfe0c44f57c99eda
ralph_abbracx <tankoraphael@gmail.com>
2019-09-06 06:41:40 +0100

    changes on feature branch

--- web/urls.py
+++ web/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('mywebsite/', include("mywebsite.urls"))
+    path('mywebsite/', include("mywebsite.urls")),
     path('admin/', admin.site.urls),
 ]
